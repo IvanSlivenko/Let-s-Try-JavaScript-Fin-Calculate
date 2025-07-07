32ed0be16672f511d9f0b1a1d89bbcd374d15bff
IvanSlivenko <ivan8822@ukr.net>
2025-07-07 12:54:39 +0300

    07-07-2025-12-54

--- project/client/src/pages/Layout.tsx
+++ project/client/src/pages/Layout.tsx
@@ -1,11 +1,12 @@
 import { type FC } from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout: FC = () => {
   return (
     <div className='min-h-screen bg-slate-900 font-roboto text-white pb-20' >
       <div>HEADER</div>
       <div className="container">
-        
+        <Outlet/>
       </div>
     </div>
   )
