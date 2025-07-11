6827f09c673fc4a4e294485679f383ff8d0d994c
IvanSlivenko <ivan8822@ukr.net>
2025-07-11 15:03:12 +0300

    11-07-2025-15-03

--- project/client/src/servises/auth.service.ts
+++ project/client/src/servises/auth.service.ts
@@ -1,8 +1,13 @@
-import type { IUserData } from "../types/types";
+import { instance } from "../api/axios.api";
+import type { IResponseUserData, IUserData } from "../types/types";
 
 export const AuthService = {
-    async registration(userData: IUserData): Promise<>{},
-    async login(){},
-    async getMe(){}
-}
-
+  async registration(
+    userData: IUserData
+  ): Promise<IResponseUserData | undefined> {
+    const {data} = await instance.post<IResponseUserData>('user', userData) 
+    return data
+  },
+  async login() {},
+  async getMe() {},
+};
--- project/client/src/types/types.ts
+++ project/client/src/types/types.ts
@@ -4,13 +4,13 @@ export interface IUserData {
 }
 
 export interface IResponseUserData{
-    email: string,
-    password: string,
-    createdAt: string,
-    updatedAt: string,
-    __v?: number,
-    _id?: string,
-    message: string
+    email: string | undefined,
+    password: string | undefined,
+    createdAt: string | undefined,
+    updatedAt: string | undefined,
+    __v?: number | undefined,
+    _id?: string | undefined,
+    message: string | undefined
 
 
 }
