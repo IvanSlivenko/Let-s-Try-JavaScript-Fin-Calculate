2464ee41e6285a797e7ca478f1e28e510ba24663
IvanSlivenko <ivan8822@ukr.net>
2025-07-14 00:15:40 +0300

    14-07-2025-0-15

--- project/client/src/pages/Categories.tsx
+++ project/client/src/pages/Categories.tsx
@@ -1,14 +1,29 @@
 import type { FC } from "react";
+import { AiFillEdit, AiFillCloseCircle } from "react-icons/ai";
+import { Form } from "react-router-dom";
 
 const Categories: FC = () => {
-  return <div className="mt-10 p-4 rounded-md bg-slate-800  ">
-    <h1> Your category list: </h1>
-    <div className="flex mt-2 items-center gap-2 flex-wrap ">
-      <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
-        <div className="absolute px-3 left-0 top-0 bottom-0 right-0 rounded-lg bg-black/90 items-center "></div>
+  return (
+    <div className="mt-10 p-4 rounded-md bg-slate-800  ">
+      <h1> Your category list: </h1>
+      <div className="flex mt-2 items-center gap-2 flex-wrap ">
+        <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
+          Salary
+          <div className="absolute px-3 left-0 top-0 bottom-0 right-0 rounded-lg bg-black/90 flex items-center justify-between ">
+            <button>
+              <AiFillEdit />
+            </button>
+            <Form className="flex " method="delete" action="/categories">
+              <input type="hidden" value={"Category ID"} />
+              <button type="submit">
+                  <AiFillCloseCircle/>
+              </button>
+            </Form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Categories;
