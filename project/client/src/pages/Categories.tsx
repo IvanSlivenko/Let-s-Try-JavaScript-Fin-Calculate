d6ee488ceacb05ca0f4f1d02052a770198bcde97
IvanSlivenko <ivan8822@ukr.net>
2025-07-14 20:31:06 +0300

    14-07-2025-20-31

--- project/client/src/pages/Categories.tsx
+++ project/client/src/pages/Categories.tsx
@@ -1,15 +1,27 @@
 import type { FC } from "react";
 import { AiFillEdit, AiFillCloseCircle } from "react-icons/ai";
+import { FaPlus} from 'react-icons/fa'
 import { Form } from "react-router-dom";
+import CategoryModal from "../components/CategoryModal";
+
+export const categoriesAction = async ({ request }) => {
+  switch(request.method){
+    case "POST": {}
+    case "PATCH": {}
+    case "DELETE": {}
+  }
+}
 
 const Categories: FC = () => {
   return (
+    <>
     <div className="mt-10 p-4 rounded-md bg-slate-800  ">
       <h1> Your category list: </h1>
+      {/* Category List */}
       <div className="flex mt-2 items-center gap-2 flex-wrap ">
         <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
           Salary
-          <div className="absolute px-3 left-0 top-0 bottom-0 right-0 rounded-lg bg-black/90 flex items-center justify-between ">
+          <div className="absolute hidden px-3 left-0 top-0 bottom-0 right-0 rounded-lg bg-black/90 items-center justify-between group-hover:flex">
             <button>
               <AiFillEdit />
             </button>
@@ -22,7 +34,16 @@ const Categories: FC = () => {
           </div>
         </div>
       </div>
+
+      {/* Add Category */}
+      <button className="max-w-fit flex items-center gap-2 text-white/50  mt-5 hover:text-white">
+        <FaPlus/>
+        <span>Create a new category</span>
+      </button>
     </div>
+
+    <CategoryModal type="post" id={1} setVisibleModal={()=>{}}/>
+    </>
   );
 };
 
